041954205939dfe26a019b77554b29ee2f6547da
Dan Kenny <dk@amp.co>
2016-12-07 11:01:37 +0000

    es6ify sample test

--- generators/app/templates/spec/list.spec.js
+++ generators/app/templates/spec/list.spec.js
@@ -4,7 +4,7 @@ import * as Redux from "redux/lib/index";
 import { reducers } from '../app/reducers/reducers';
 
 
-var DATA = {
+const list = {
     items: [{
         id: 'item-1',
         title: 'Item 1',
@@ -22,19 +22,23 @@ var DATA = {
     }]
 };
 
-var store = Redux.createStore(reducers, DATA);
+const store = Redux.createStore(reducers, list);
 
 
-describe('the list reducer', function() {
-    it('should NOT have a selected item', function() {
+describe('the list reducer', () => {
+
+    it('should NOT have a selected item', () => {
+
         expect(store.getState().items[0].selected).not.toBe(true);
     });
 
-    it('should be able to select an item', function() {
+    it('should be able to select an item', () => {
+
         store.dispatch({
             id: 'item-1',
             type: 'TOGGLE_ME'
         });
+
         expect(store.getState().items[0].selected).toBe(true);
     });
 });
